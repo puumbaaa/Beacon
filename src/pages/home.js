ef2fef2120a76d1222ecef7a4ed3906099c40954
Spades749 <mfournier@gaming.tech>
2024-06-12 17:08:38 +0200

    grosse update search bar + autre chose css

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,33 +1,44 @@
-import React from 'react';
-import { Container, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Navbar, Nav, Form, FormControl, Button, Dropdown } from 'react-bootstrap';
 import '../pages_style/home_style.css';
 import { Helmet } from 'react-helmet';
 
 function Home() {
+    const [region, setRegion] = useState('EUW');
+
     return (
         <>
             <Helmet>
                 <title>Beacon</title>
                 <meta name="description" content="This is a website to improve your level on League of Legends" />
-                <meta content="no-cache" />
+                <meta http-equiv="pragma" content="no-cache" />
                 <meta charSet="UTF-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="shortcut icon" href="../img/logo_beacon.png" />
+                <link rel="shortcut icon" href="/img/logo_beacon.png" />
             </Helmet>
 
             <div id="wallpaper"></div>
 
             <Navbar className="navbar-custom" expand="lg" fixed="top">
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand href="#home" className="brand-left">
                         <img
-                            src="../img/logo_beacon.png"
-                            width="30"
-                            height="30"
+                            src="/img/logo_beacon.png"
+                            width="60"
+                            height="60"
                             className="d-inline-block align-top"
                             alt="Beacon logo"
+                            draggable="false"
                         />
-                        <span>BEACON</span>
+                        <span>
+                            <img
+                                src="/img/Beacon_Title.png"
+                                width="100"
+                                height="25"
+                                alt="Beacon Title"
+                                draggable="false"
+                            />
+                        </span>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
@@ -41,11 +52,12 @@ function Home() {
                         <Nav>
                             <Nav.Link href="#profile">
                                 <img
-                                    src="../img/login_img.png"
-                                    width="30"
-                                    height="30"
+                                    src="/img/login_img.png"
+                                    width="40"
+                                    height="40"
                                     className="d-inline-block align-top"
                                     alt="Login"
+                                    draggable="false"
                                 />
                             </Nav.Link>
                         </Nav>
@@ -55,39 +67,36 @@ function Home() {
 
             <div className="content-container">
                 <Container className="content">
-                    <img src="../img/Beacon_Title.png" alt="BEACON" />
+                    <img className="title-image" src="/img/Beacon_Title.png" alt="BEACON" draggable="false" />
                     <Form className="search-bar">
                         <FormControl
                             type="search"
                             placeholder="Search for yourself"
-                            className="me-2"
+                            className="search-input"
                             aria-label="Search"
                         />
-                        <select className="form-control">
-                            <option>EUW</option>
-                            <option>NA</option>
-                            <option>KR</option>
-                            <option>EUN</option>
-                            <option>JP</option>
-                            <option>LA</option>
-                            <option>OC</option>
-                            <option>TR</option>
-                            <option>RU</option>
-                            <option>PH</option>
-                            <option>SG</option>
-                            <option>TH</option>
-                            <option>TW</option>
-                            <option>VN</option>
-                        </select>
-                        <Button variant="outline-success">
-                            <img src="../img/loop.png" alt="loop_button_img" />
+                        <Dropdown className="region-dropdown">
+                            <Dropdown.Toggle variant="success" id="dropdown-basic" className="region-badge">
+                                {region}
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                {['EUW', 'NA', 'KR', 'EUN', 'JP', 'LA', 'OC', 'TR', 'RU', 'PH', 'SG', 'TH', 'TW', 'VN'].map(r => (
+                                    <Dropdown.Item key={r} onClick={() => setRegion(r)}>
+                                        {r}
+                                    </Dropdown.Item>
+                                ))}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Button variant="outline-success" className="search-button">
+                            <img src="/img/loop.png" alt="loop_button_img" draggable="false" />
                         </Button>
                     </Form>
                 </Container>
             </div>
 
             <div className="language-switch">
-                <img src="../img/french_flag.png" alt="French" />
+                <img src="/img/french_flag.png" alt="French" draggable="false" />
             </div>
         </>
     );
