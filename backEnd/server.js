36faacc7463cb7573e519475a7a1e4451ad3a406
MomoCrash <44437470+MomoCrash@users.noreply.github.com>
2024-06-13 19:02:25 +0200

    Connected backend to front end

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,30 +1,40 @@
 const express = require('express');
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-const db = mysql.createConnection( {
-    "host": "localhost",
-    "user": "superuser",
-    "password": "ZmEUIgrj_7bA6WxI",
-    "database": "beacon"
-})
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: "localhost",
+    user: "superuser",
+    password: "ZmEUIgrj_7bA6WxI",
+    database: "beacon"
+});
 
-app.get("/", (req, res) => {
-    return res.json("From Backend Side")
-})
-
-// Exmple of db query from backend to front
 app.get("/users", (req, res) => {
     const sql = "SELECT * FROM users";
-    db.query(sql, (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    })
-})
+
+    // Get a connection from the pool
+    pool.getConnection((err, connection) => {
+        if (err) console.error(err);
+
+        // Execute the query
+        connection.query(sql, (err, data) => {
+            // Release the connection back to the pool
+            connection.release();
+
+            if (err) {
+                console.error('Error executing query:', err);
+                return res.status(500).json({ error: "Query execution error", details: err.message });
+            }
+
+            return res.json(data);
+        });
+    });
+});
 
 app.listen(8081, () => {
-    console.log("Listening")
-})
+    console.log("Listening on port 8081");
+});
--- frontEnd/src/handlers/sql_handler.jsx
+++ frontEnd/src/handlers/sql_handler.jsx
@@ -1,4 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-export class SQLHandler extends React.Component {
+export function SQLHandler() {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:8081/users")
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(err => console.error(err))
+    }, [])
+
+    return (
+        <div>
+            <table>
+                <thead>
+                <th>ID</th>
+                <th>KDA</th>
+                </thead>
+                <tbody>
+                {data.map((d, i) => (
+                    <tr key={i}>
+                        <td> {d.id} </td>
+                        <td> {d.kda} </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
