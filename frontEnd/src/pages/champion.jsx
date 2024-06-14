e78e80f349a85c48e7e1551d406581388b52fbf5
MomoCrash <44437470+MomoCrash@users.noreply.github.com>
2024-06-14 18:12:27 +0200

    Update champion.jsx

--- frontEnd/src/pages/champion.jsx
+++ frontEnd/src/pages/champion.jsx
@@ -66,12 +66,3 @@ export default function ChampGuid() {
     );
     
 }
-
-export default function ChampGuid(){
-    let pathname = window.location.pathname.split('/guide/');
-    return(
-        <div>
-            <h1>{pathname}</h1>
-        </div>
-    )
-}
