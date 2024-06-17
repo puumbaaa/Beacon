e5fc6864660b3047281d5dd87475b5e3b4394129
MomoCrash <44437470+MomoCrash@users.noreply.github.com>
2024-06-17 15:48:32 +0200

    login page

--- frontEnd/src/pages/login.jsx
+++ frontEnd/src/pages/login.jsx
@@ -1,55 +1,58 @@
-import { Container, Row, Col, Card, CardBody, Button, Form } from'react-bootstrap';
+import {Container, Row, Col, Card, CardBody, Button, Form, Image} from 'react-bootstrap';
+import "../pages_style/login.css"
+import React from "react";
 
 export default function Login() {
     return (
-        <Container fluid className='my-5'>
+        <Container fluid style={{margin: "100px"}}>
+
+            <div id="wallpaper"></div>
 
             <Row className='g-0 align-items-center'>
-                <Col col='6'>
+                <Col xs={6} md={4}>
 
-                    <Card className='my-5 cascading-right' style={{background: 'hsla(0, 0%, 100%, 0.55)',  backdropFilter: 'blur(30px)'}}>
+                    <img src="/img/Beacon_Title.png"/>
+
+                    <Card className='my-5 cascading-right'
+                          style={{background: 'hsla(0, 0%, 100%, 0.55)', backdropFilter: 'blur(30px)'}}>
                         <CardBody className='p-5 shadow-5 text-center'>
 
-                            <h2 className="fw-bold mb-5">Enregistrez-vous !</h2>
+                            <h2 className="fw-bold mb-5">Sign up now</h2>
 
-                            <Row>
-                                <Col col='6'>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Adresse email</Form.Label>
+                                <Form.Control type="email" placeholder="beacon@example.com"/>
+                            </Form.Group>
 
-                                    <input id='form1' type='text'/>
-                                </Col>
-
-                                <Col col='6'>
-                                    <input id='form2' type='text'/>
-                                </Col>
-                            </Row>
-
-                            <input id='form3' type='email'>
-                            <Form.Label htmlFor="inputPassword5"> Mot de passe <Form.Label/>
+                            <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                id="inputPassword5"
+                                aria-describedby="passwordHelpBlock"
+                                placeholder="Your password"
+                            />
 
                             <div className='d-flex justify-content-center mb-4'>
-                                <input type="checkbox" name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
+                                <input type="checkbox" name='flexCheck' value='' id='flexCheckDefault'
+                                       label='Subscribe to our newsletter'/>
                             </div>
 
                             <Button className='w-100 mb-4' size='md'>sign up</Button>
 
                             <div className="text-center">
 
-                                <p>or sign up with:</p>
+                                <p>Ou connectez-vous avec :</p>
 
-                                <Button tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                                    FB
+                                <Button tag='a' color='none' className='mx-3' style={{color: '#1266f1'}}>
+                                    <div icon='twitter' size="sm"/>
                                 </Button>
 
-                                <Button tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                                    TW
+                                <Button tag='a' color='none' className='mx-3' style={{color: '#1266f1'}}>
+                                    <div icon='google' size="sm"/>
                                 </Button>
 
-                                <Button tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                                    GO
-                                </Button>
-
-                                <Button tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                                    GIT
+                                <Button tag='a' color='none' className='mx-3' style={{color: '#1266f1'}}>
+                                    <div icon='github' size="sm"/>
                                 </Button>
 
                             </div>
@@ -59,8 +62,8 @@ export default function Login() {
                 </Col>
 
                 <Col col='6'>
-                    <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" className="w-100 rounded-4 shadow-4"
-                         alt=""/>
+                    <Image src="/img/background.jpg" width={400} height={700} className="rounded-4 shadow-4"
+                           alt=""/>
                 </Col>
 
             </Row>
